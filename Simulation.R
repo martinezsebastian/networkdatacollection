0ac2101d4787bfcb74784f0beefd4a8c2b531e17
Sebastian Martinez <sebastian.martinez32@gmail.com>
2018-09-14 13:16:48 +0200

    Updating abstract

--- Simulation.R
+++ Simulation.R
@@ -1,3 +1,12 @@
+###### ------------------------------------------------------------
+###### ------------------------------------------------------------
+###### NAME: Network Data Collection
+###### DATE: September 2018
+###### Version: 1
+###### ------------------------------------------------------------
+###### ------------------------------------------------------------
+###### Simulation of close relationships and time-based relationships
+
 
 if(!require(FNN)) install.packages("FNN",repos = "http://cran.us.r-project.org")
 library("FNN")
@@ -8,7 +17,15 @@ library("reshape")
 if(!require(igraph)) install.packages("igraph",repos = "http://cran.us.r-project.org")
 library("igraph")
 
-nodes <- 100
+if(!require(network)) install.packages("network",repos = "http://cran.us.r-project.org")
+library("network")
+
+if(!require(sna)) install.packages("sna",repos = "http://cran.us.r-project.org")
+library("sna")
+if(!require(ggnet)) devtools::install_github("briatte/ggnet")
+library("ggnet")
+
+nodes <- 1000
 n_rel <- runif(n = nodes, min = 0, max = 1)
 n_tim <- runif(n = nodes, min = 0, max = 1)
 
@@ -21,8 +38,14 @@ neighbor_rel_melt <- reshape::melt(neighbor_rel, id.vars = c("id"))
 neighbor_rel_melt <- neighbor_rel_melt[c("id", "value")]
 neighbor_rel_melt$id <- as.integer(neighbor_rel_melt$id)
 
+net <- network::network(x = neighbor_rel_melt, directed = FALSE)
+a <-   network::as.matrix.network.adjacency(x = net)
 
-g <- igraph::graph.edgelist(as.matrix(neighbor_rel_melt), directed = FALSE)
-plot(g)
+
+network.vertex.names(net) <- neighbor_rel$id
+net$id <- neighbor_rel$id
+net$deg <- sna::degree(net)
+
+ggnet2(net, node.size = 2, edge.size = 0.5, edge.color = "gray")
 
 
